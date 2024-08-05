e6d10671f76b2fd4ae0b94cf5f50ac3e3c8b557d
Naveen142005 <142866508+Naveen142005@users.noreply.github.com>
2024-08-05 14:53:15 +0530

    [2053] [Time Beats: 59.81%] [Memory Beats: 14.46%] - LeetPush

--- 2053-Kth-Distinct-String-in-an-Array.cpp
+++ 2053-Kth-Distinct-String-in-an-Array.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-
+    struct Mymap {
+        string s;
+        int c;
+    };
     string kthDistinct(vector<string>& arr, int k) {
-        unordered_map<string, int> mp;
         ios_base::sync_with_stdio(false);
-        for (auto& i : arr) 
-            if (!mp[i]) mp[i] =1;
-            else mp[i]++;
+        unordered_map<string, int> mp;
+        struct Mymap map__[arr.size()];
+       
+        for (int i = 0; i < arr.size(); i++) mp[arr[i]] = -1 , map__[i].c = -1;
         
+        for (int i = 0; i < arr.size(); i++) 
+            if (mp[arr[i]] == -1) 
+                mp[arr[i]] = i, map__[i].s = arr[i] , map__[i].c = 1;
+            else 
+                map__[mp[arr[i]]].c += 1;
+            
+        string ans = "";
 
-        for (auto& i : arr) {
-            if (mp[i] == 1) k--;
-            if (!k) return i;
+        for (int i = 0 ; (i < arr.size() && k > 0) ; i++ ) {
+            if (map__[i].c == 1)
+                k--;
+            if (!k) 
+                ans = map__[i].s;
         }
-        return "";
+        return ans;
     }
 };
