c7ca3413fd71138f1c032df74aaedb28edd5b804
Naveen142005 <142866508+Naveen142005@users.noreply.github.com>
2026-06-05 18:17:59 +0530

    [3753] [Time Beats: 13.56%] [Memory Beats: 22.03%] - LeetPush

--- Total-Waviness-of-Numbers-in-Range-II.cpp
+++ Total-Waviness-of-Numbers-in-Range-II.cpp
@@ -1,41 +1,41 @@
 1class Solution {
 2public:
 3    long long dp[16][2][16][16][2][20];
-4    long long solve(string& s, int tight, int idx, int last, int slast, int lz, int crr) {
+4    long long solve(string& s, int tt, int idx, int last, int slast, int lz, int crr) {
 5
 6        if (idx == s.size())
 7            return crr;
 8
-9        if (last >= 0 && slast >= 0 && dp[idx][tight][last][slast][lz][crr] != -1) 
-10            return dp[idx][tight][last][slast][lz][crr];
+9        if (last >= 0 && slast >= 0 && dp[idx][tt][last][slast][lz][crr] != -1) 
+10            return dp[idx][tt][last][slast][lz][crr];
 11
-12        int lim = (tight) ? s[idx] - '0' : 9;
+12        int lim = (tt) ? s[idx] - '0' : 9;
 13        long long cnt = 0;
 14
 15        for (int i = 0; i <= lim; i += 1) {
-16            int nxt_tight = (tight && (s[idx] - '0') == i);
+16            int nxt_tt = (tt && (s[idx] - '0') == i);
 17            int nxt_lz = (lz && !i);
 18
 19            if (i == 0 && lz) {
-20                cnt += solve(s, nxt_tight, idx + 1, last, slast, 1, crr);
+20                cnt += solve(s, nxt_tt, idx + 1, last, slast, 1, crr);
 21                continue;
 22            }
 23            if (last == -1) {
-24                cnt += solve(s, nxt_tight, idx + 1, i, -1, nxt_lz, crr);
+24                cnt += solve(s, nxt_tt, idx + 1, i, -1, nxt_lz, crr);
 25                continue;
 26            }
 27
 28            if (slast == -1) {
-29                cnt += solve(s, nxt_tight, idx + 1, last, i, nxt_lz, crr);
+29                cnt += solve(s, nxt_tt, idx + 1, last, i, nxt_lz, crr);
 30                continue;
 31            }
 32
 33            int okva = ((last < slast && slast > i) || (last > slast && slast < i));
 34
-35            cnt += solve(s, nxt_tight, idx + 1, slast, i, nxt_lz, crr + okva);
+35            cnt += solve(s, nxt_tt, idx + 1, slast, i, nxt_lz, crr + okva);
 36        }
 37        if (last >= 0 && slast >= 0) 
-38            return dp[idx][tight][last][slast][lz][crr] = cnt;
+38            return dp[idx][tt][last][slast][lz][crr] = cnt;
 39        return cnt;
 40    }
 41
